a3ad092e522a1266aa0228ddd8e5517014e2aef7
Sid-op777 <69779993+Sid-op777@users.noreply.github.com>
2025-04-24 21:14:05 +0530

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,114 @@
-import Image from "next/image";
+'use client';
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { motion } from 'framer-motion';
+
+// Define an interface for the form data
+interface FormData {
+  longUrl: string;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [shortenedUrl, setShortenedUrl] = useState('');
+  const { register, handleSubmit } = useForm<FormData>();
+  const [isCopied, setIsCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); // Add loading state
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    setIsLoading(true); // Set loading state to true on submit
+
+    try {
+      const response = await fetch(
+        `http://localhost:8080/shorten?longUrl=${encodeURIComponent(data.longUrl)}&life=P1M`
+        , {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    setShortenedUrl('http:localhost:8080/'+result.id); //Assuming short URL is in result.shortUrl
+    } catch (error) {
+      console.error('Error shortening URL:', error);
+      // Handle error (e.g., display an error message)
+    } finally {
+      setIsLoading(false); // Set loading state back to false after API call
+    }
+  };
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(shortenedUrl);
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 2000); // Reset after 2 seconds
+  };
+
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      {/* Header Section */}
+      <header className="bg-white shadow-md py-4 px-6 w-full">
+        <div className="container mx-auto flex items-center">
+          <Image
+            src="/assets/logo.svg"
+            alt="LinkToolkit Logo"
+            width={32}
+            height={32}
+          />
+          <h1 className="text-xl font-semibold text-gray-800 ml-2">LinkToolkit</h1>
         </div>
+      </header>
+
+      {/* Main Content Section */}
+      <main className="flex flex-col items-center justify-center flex-grow w-full max-w-md mx-auto">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col items-center space-y-4 w-full"
+        >
+          <div>
+            <input
+              type="url"
+              {...register('longUrl', { required: true })}
+              placeholder="Paste a long URL"
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-link-blue shadow-md"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full py-2 px-4 bg-blue-400 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            >
+              {isLoading ? 'Loading...' : 'Shorten'} {/* Conditionally render button text */}
+            </button>
+          </div>
+        </form>
+
+        {/* Shortened URL Display */}
+        {shortenedUrl && (
+          <div className="mt-6 w-fit relative">
+            <input
+              type="text"
+              value={shortenedUrl}
+              readOnly
+              className="w-full px-4 py-2 pr-18 border rounded-md focus:outline-none shadow-lg text-gray-700"
+              style={{ borderColor: '#54A2FC' }}
+            />
+            <div className="absolute top-0 right-0 h-full flex items-center pr-2">
+              <button
+                onClick={handleCopyClick}
+                className="bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+              >
+                {isCopied ? 'Copied!' : 'Copy'}
+              </button>
+            </div>
+          </div>
+        )}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
